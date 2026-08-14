16cbe41f56dc34f14dbc0529e4fc4e6543977149
Harshil Khatri <khatriharshil26@gmail.com>
2026-08-14 13:30:27 +0530

    Add search feature in API.

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,8 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from core.models import Note
 from core.serializers import NoteSerializer
+from rest_framework import filters
 
 class NoteAPI(ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
-    
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title']
